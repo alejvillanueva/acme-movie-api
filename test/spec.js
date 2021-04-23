cdd62b87795a913c7c52bb23795f1b6477226e74
Alejandra Villanueva <alejyvilla@gmail.com>
2021-04-22 22:02:55 -0400

    All models are loaded test

--- test/spec.js
+++ test/spec.js
@@ -1,4 +1,8 @@
 const { expect } = require('chai');
+const {
+  syncAndSeed,
+  models: { Actor, Movie, Role },
+} = require('../db');
 const app = require('supertest')(require('../app'));
 
 describe('Basic Test', () => {
@@ -17,5 +21,20 @@ describe('Routes', () => {
       expect(response.status).to.equal(200);
       expect(response.text).to.include('The Acme Api');
     });
+  });
+});
+
+describe('Sequelize', () => {
+  beforeEach(async () => {
+    await syncAndSeed();
+  });
+
+  it('loads all the models', async () => {
+    const movies = await Movie.findAll();
+    const actors = await Actor.findAll();
+    const roles = await Role.findAll();
+    expect(movies.length).to.be.greaterThan(0);
+    expect(actors.length).to.be.greaterThan(0);
+    expect(roles.length).to.be.greaterThan(0);
   });
 });
